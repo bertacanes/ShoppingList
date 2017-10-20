8dee54086e0ee675d0276b556a5906e393f637f3
berta.canes <bertacanesbadia@gmail.com>
2017-10-20 15:54:28 +0200

    Can add items to the list

--- app/src/main/java/edu/upc/eseiaat/pma/shoppinglistapplication/ShoppingListActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/shoppinglistapplication/ShoppingListActivity.java
@@ -2,6 +2,7 @@ package edu.upc.eseiaat.pma.shoppinglistapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -39,7 +40,20 @@ public class ShoppingListActivity extends AppCompatActivity {
                 itemList
         );
 
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addItem();
+            }
+        });
+
         list.setAdapter(adapter);
 
+    }
+
+    private void addItem(){
+        String item_text = edit_item.getText().toString();
+        itemList.add(item_text);
+        adapter.notifyDataSetChanged();
     }
 }
