150f314cf8f3c807f3fb859ffaf1dce36754fca4
berta.canes <bertacanesbadia@gmail.com>
2017-10-20 16:04:47 +0200

    Can add items to the list through the keyboard

--- app/src/main/java/edu/upc/eseiaat/pma/shoppinglistapplication/ShoppingListActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/shoppinglistapplication/ShoppingListActivity.java
@@ -2,11 +2,13 @@ package edu.upc.eseiaat.pma.shoppinglistapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -46,6 +48,13 @@ public class ShoppingListActivity extends AppCompatActivity {
                 addItem();
             }
         });
+        edit_item.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                addItem();
+                return true;
+            }
+        });
 
         list.setAdapter(adapter);
 
@@ -53,7 +62,10 @@ public class ShoppingListActivity extends AppCompatActivity {
 
     private void addItem(){
         String item_text = edit_item.getText().toString();
-        itemList.add(item_text);
-        adapter.notifyDataSetChanged();
+        if (!item_text.isEmpty()) {
+            itemList.add(item_text);
+            adapter.notifyDataSetChanged();
+            edit_item.setText("");
+        }
     }
 }
