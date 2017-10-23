67857ca6c08dbac08ddbb69f4f1f9cdcc4e6365d
berta.canes <bertacanesbadia@gmail.com>
2017-10-23 16:01:14 +0200

    App saves changes even when you close the app and reopen it

--- app/src/main/java/edu/upc/eseiaat/pma/shoppinglistapplication/ShoppingListActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/shoppinglistapplication/ShoppingListActivity.java
@@ -16,6 +16,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -24,6 +25,7 @@ import java.util.ArrayList;
 public class ShoppingListActivity extends AppCompatActivity {
 
     private static final String FILENAME = "shopping_list.txt";
+    private static final int MAX_BYTES = 8000;
 
     private ArrayList<ShoppingItem> itemList;
     private ShoppingListAdapter adapter;
@@ -50,6 +52,27 @@ public class ShoppingListActivity extends AppCompatActivity {
         }
     }
 
+    private void readItemList(){
+        itemList = new ArrayList<>();
+        try {
+            FileInputStream fis = openFileInput(FILENAME);
+            byte[] buffer = new byte[MAX_BYTES];
+            int nread = fis.read(buffer);
+            String content = new String(buffer, 0, nread);
+            String[] lines = content.split("\n");
+            for (String line : lines) {
+                String[] parts = line.split(";");
+                itemList.add(new ShoppingItem(parts[0], parts[1].equals("true")));
+            }
+            fis.close();
+        } catch (FileNotFoundException e) {
+            Log.i("berta", "readItemList: FileNotFoundException");
+        } catch (IOException e) {
+            Log.i("berta", "readItemList: IOEException");
+            Toast.makeText(this, R.string.cannot_read, Toast.LENGTH_SHORT).show();
+        }
+    }
+
     @Override
     protected void onStop() {
         super.onStop();
@@ -65,11 +88,7 @@ public class ShoppingListActivity extends AppCompatActivity {
         btn_add = (Button) findViewById(R.id.btn_add);
         edit_item = (EditText) findViewById(R.id.edit_item);
 
-        itemList = new ArrayList<>();
-        itemList.add(new ShoppingItem("Potatoes"));
-        itemList.add(new ShoppingItem("Toilet roll"));
-        itemList.add(new ShoppingItem("Carrots"));
-        itemList.add(new ShoppingItem("Chocolate"));
+        readItemList();
 
         adapter = new ShoppingListAdapter(
                 this,
